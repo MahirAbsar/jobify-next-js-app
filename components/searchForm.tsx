74b73ebb6d4c28cef134f383cfab80ac01fe8cad
Mahir <mahirabsar181@gmail.com>
2024-03-17 03:33:51 +0600

    feat: implement search job logic

--- components/searchForm.tsx
+++ components/searchForm.tsx
@@ -11,20 +11,36 @@ import {
 } from "./ui/select";
 import { JobStatus } from "@/utils/types";
 import { Button } from "./ui/button";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const SearchForm = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    console.log(formData.get("search"), formData.get("jobStatus"));
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+    let params = new URLSearchParams();
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+    router.push(`${pathname}?${params}`);
   };
   return (
     <form
       onSubmit={handleSubmit}
       className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
     >
-      <Input type="text" name="search" placeholder="Search job" />
-      <Select name="jobStatus">
+      <Input
+        type="text"
+        name="search"
+        placeholder="Search job"
+        defaultValue={search}
+      />
+      <Select name="jobStatus" defaultValue={jobStatus}>
         <SelectTrigger>
           <SelectValue />
         </SelectTrigger>
