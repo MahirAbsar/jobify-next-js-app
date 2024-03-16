1bfc47dff8b97ae8a4c0304225972f7325ac6675
Mahir <mahirabsar181@gmail.com>
2024-03-16 19:42:37 +0600

    feat: implement job model, implement action create job

---
+++ utils/actions.ts
@@ -0,0 +1,26 @@
+"use server"
+
+import { redirect } from "next/navigation";
+import { JobType, createAndEditJobSchema } from "./types";
+import { auth } from "@clerk/nextjs";
+import prisma from "./db";
+
+function authenticateAndRedirect() {
+    const { userId } = auth()
+    if(!userId) redirect("/");
+    return userId
+}
+
+export async function createJobAction(values: JobType):Promise<JobType | null> {
+    const userId = authenticateAndRedirect();
+    try {
+        createAndEditJobSchema.parse(values);
+        const job = await prisma.job.create({
+            data: { ...values, clerkId: userId }
+        })
+        return job;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
