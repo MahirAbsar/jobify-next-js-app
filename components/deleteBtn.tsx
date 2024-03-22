11d4051376441c7ab94f865f21c40b924f6dbdc7
Mahir <mahirabsar181@gmail.com>
2024-03-22 12:18:35 +0600

    feat: implement delete job functionality

--- components/deleteBtn.tsx
+++ components/deleteBtn.tsx
@@ -1,4 +1,26 @@
-const DeleteBtn = () => {
-  return <h1 className="text-4xl">DeleteBtn</h1>;
+import { deleteJobAction } from "@/utils/actions";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "./ui/use-toast";
+import { Button } from "./ui/button";
+
+const DeleteBtn = ({ id }: { id: string }) => {
+  const queryClient = useQueryClient();
+  const { isPending, mutate } = useMutation({
+    mutationFn: (id: string) => deleteJobAction(id),
+    onSuccess: (data) => {
+      if (!data) {
+        toast({
+          description: "There was an error",
+        });
+        return;
+      }
+      queryClient.invalidateQueries({ queryKey: ["jobs"] });
+    },
+  });
+  return (
+    <Button size="sm" disabled={isPending} onClick={() => mutate(id)}>
+      {isPending ? "deleting..." : "delete"}
+    </Button>
+  );
 };
 export default DeleteBtn;
--- components/jobCard.tsx
+++ components/jobCard.tsx
@@ -39,7 +39,7 @@ const JobCard = ({ job }: { job: JobType }) => {
         <Button asChild size="sm">
           <Link href={`/jobs/${job.id}`}>edit</Link>
         </Button>
-        <DeleteBtn />
+        <DeleteBtn id={job.id} />
       </CardFooter>
     </Card>
   );
